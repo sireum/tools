be3d9c7da3d6b584759bd13d1b51ed63a2eccadd
Robby <robby@k-state.edu>
2018-03-23 19:29:45 -0500

    Download license.txt in test.sh.

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ rm -fR runtime slang cli mill-standalone versions.properties out
 curl -Lo mill-standalone http://files.sireum.org/mill-standalone
 chmod +x mill-standalone
 curl -Lo versions.properties https://raw.githubusercontent.com/sireum/kekinian/master/versions.properties
+curl -Lo license.txt https://raw.githubusercontent.com/sireum/kekinian/master/license.txt
 git clone --depth 1 https://github.com/sireum/runtime
 git clone --depth 1 https://github.com/sireum/slang
 git clone --depth 1 https://github.com/sireum/cli
