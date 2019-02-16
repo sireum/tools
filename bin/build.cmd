fe588dd429a865dcab16259617652570aa06090e
Robby <robby@k-state.edu>
2019-02-16 13:46:16 -0600

    Reworked build, test, and publish scripts to use Slash -- universal Slang shell script (/build).

---
+++ bin/build.cmd
@@ -0,0 +1,143 @@
+::#! 2> /dev/null                                              #
+@ 2>/dev/null # 2>nul & echo off & goto BOF                    #
+exec $(cd `dirname $0` && pwd)/sireum slang run -s "$0" "$@"   #
+:BOF
+%~dp0sireum.bat slang run -s "%0" %*
+exit /B %errorlevel%
+::!#
+// #Sireum
+import org.sireum._
+
+def usage(): Unit = {
+  println("Sireum Tools /build")
+  println("Usage: ( compile | test | m2 | jitpack )+")
+}
+
+if (Os.cliArgs.size < 2) {
+  usage()
+  Os.exit(0)
+}
+
+val homeBin = Os.path(Os.cliArgs(0))
+val home = homeBin.up
+val sireumJar = homeBin / "sireum.jar"
+val mill = homeBin / "mill.bat"
+
+def downloadMill(): Unit = {
+  if (!mill.exists) {
+    println("Downloading mill ...")
+    mill.downloadFrom("http://files.sireum.org/mill-standalone")
+    mill.chmod("+x")
+    println()
+  }
+}
+
+def downloadLicense(): Unit = {
+  val license = home / "license.txt"
+  if (!license.exists) {
+    println("Downloading license ...")
+    license.downloadFrom("https://raw.githubusercontent.com/sireum/kekinian/master/license.txt")
+    println()
+  }
+}
+
+def clone(repo: String): Unit = {
+  if (!(home / repo).exists) {
+    Os.proc(ISZ("git", "clone", "--depth=1", s"https://github.com/sireum/$repo")).at(home).console.runCheck()
+  } else {
+    Os.proc(ISZ("git", "pull", "--recurse-submodules")).at(home / repo).console.runCheck()
+  }
+  println()
+}
+
+def jitpack(): Unit = {
+  println("Triggering jitpack ...")
+  val r = Os.proc(ISZ(mill.string, "jitPack", "--owner", "sireum", "--repo", "tools")).at(home).console.run()
+  r match {
+    case r: Os.Proc.Result.Normal =>
+      println(r.out)
+      println(r.err)
+      if (!r.ok) {
+        eprintln(s"Exit code: ${r.exitCode}")
+      }
+    case r: Os.Proc.Result.Exception =>
+      eprintln(s"Exception: ${r.err}")
+    case _: Os.Proc.Result.Timeout =>
+      eprintln("Timeout")
+      eprintln()
+  }
+  println()
+}
+
+def compile(): Unit = {
+
+  def tipe(): Unit = {
+    println("Slang type checking ...")
+    Os.proc(ISZ("java", "-jar", sireumJar.string, "slang", "tipe", "--verbose", "-r", "-s", home.string)).
+      at(home).console.runCheck()
+    println()
+  }
+
+  if (Os.isMac) {
+    jitpack()
+  }
+  tipe()
+  println("Compiling ...")
+  Os.proc(ISZ(mill.string, "tools.jvm.tests.compile")).at(home).console.runCheck()
+  println()
+}
+
+def test(): Unit = {
+  compile()
+  println("Running jvm tests ...")
+  Os.proc(ISZ(mill.string, "tools.jvm.tests")).at(home).console.runCheck()
+  println()
+}
+
+def m2(): Unit = {
+  val m2s: ISZ[ISZ[String]] = for (plat <- ISZ("shared", "jvm")) yield ISZ("tools", plat, "m2")
+
+  val m2Paths: ISZ[Os.Path] =
+    for (cd <- for (m2 <- m2s) yield st"${(m2, Os.fileSep)}".render) yield  home / "out" / cd
+
+  for (m2p <- m2Paths) {
+    m2p.removeAll()
+  }
+
+  (home / "out").removeAll()
+
+  Os.proc(ISZ[String](mill.string, "all") ++ (for (m2 <- m2s) yield st"${(m2, ".")}".render)).
+    at(home).env(ISZ("SIREUM_SOURCE_BUILD" ~> "false")).console.runCheck()
+
+  val repository = Os.home / ".m2" / "repository"
+  repository.removeAll()
+
+  println()
+  println("Artifacts")
+  for (m2p <- m2Paths; p <- (m2p / "dest").overlayMove(repository, F, F, _ => T, T).values) {
+    println(s"* $p")
+  }
+  println()
+}
+
+
+downloadMill()
+
+downloadLicense()
+
+for (m <- ISZ("runtime", "slang")) {
+  clone(m)
+}
+
+for (i <- 1 until Os.cliArgs.size) {
+  Os.cliArgs(i) match {
+    case string"compile" => compile()
+    case string"test" => test()
+    case string"m2" => m2()
+    case string"jitpack" => jitpack()
+    case cmd =>
+      usage()
+      eprintln(s"Unrecognized command: $cmd")
+      Os.exit(-1)
+  }
+}
