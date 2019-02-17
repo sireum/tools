b38245ec081334ec9bdb2ac19642820336339e9a
Robby <robby@k-state.edu>
2019-02-16 19:22:03 -0600

    Removed stale scripts.

--- clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-export SCRIPT_HOME=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
-cd ${SCRIPT_HOME}
-rm -fR sireum mill-standalone versions.properties license.txt runtime slang cli out
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-export SCRIPT_HOME=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
-cd ${SCRIPT_HOME}
-./prelude.sh
-./sireum slang tipe --verbose -r -s runtime/library:slang:kekinian/cli:shared:jvm
-rm -fR out
-./mill-standalone tools.jvm.tests.test
