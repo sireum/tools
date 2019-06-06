4c0cfc3268f05f455d83c91da72a3ee2c9fa08a6
Robby <robby@k-state.edu>
2019-06-05 19:17:48 -0600

    Checkpointing BitCodecGen.

--- shared/src/main/scala/org/sireum/tools/BitCodecGen.scala
+++ shared/src/main/scala/org/sireum/tools/BitCodecGen.scala
@@ -29,8 +29,24 @@ package org.sireum.tools
 import org.sireum._
 import org.sireum.bitcodec.Spec
 import org.sireum.lang.{ast => AST}
+import org.sireum.message.Reporter
 
 object BitCodecGen {
+  val kind: String = "bcgen"
+  val beginCodeMarker: String = "// BEGIN USER CODE:"
+  val endCodeMarker: String = "// END USER CODE:"
+  val specName: ISZ[String] = ISZ("org", "sireum", "bitcodec", "Spec")
+  val unionName: ISZ[String] = specName :+ "Union"
+  val repeatName: ISZ[String] = specName :+ "Repeat"
+  val rawName: ISZ[String] = specName :+ "Raw"
+
+  val funOwners: HashSet[ISZ[String]] = HashSet.empty ++ ISZ(unionName, repeatName, rawName)
+  val funName: HashMap[ISZ[String], String] = HashMap.empty ++ ISZ(
+    unionName ~> "choice",
+    repeatName ~> "size",
+    rawName ~> "size"
+  )
+
   def gen(licenseOpt: Option[String],
           filename: String,
           packageNames: ISZ[String],
@@ -38,10 +54,59 @@ object BitCodecGen {
           text: String,
           config: Spec,
           program: AST.TopUnit.Program,
-          prevGen: String): ST = {
-    return BitCodecGen(licenseOpt, filename, packageNames, name, text, config, program, prevGen).gen()
+          prevGen: String,
+          reporter: Reporter): String = {
+    val bcGen = BitCodecGen(licenseOpt, filename, packageNames, name,
+      ops.StringOps(text).replaceAllLiterally("/r/n", "/n"), config, program,
+      ops.StringOps(prevGen).replaceAllLiterally("/r/n", "/n"), Reporter.create)
+    val r = bcGen.gen()
+    reporter.reports(bcGen.reporter.messages)
+    return r
+  }
+
+  @record class EnumFunCollector(reporter: Reporter) extends AST.MTransformer {
+    var enums: HashMap[String, AST.Stmt.Enum] = HashMap.empty
+    var funs: HashMap[String, (AST.Exp.Fun, AST.Type)] = HashMap.empty
+
+    override def preExpInvoke(o: AST.Exp.Invoke): AST.MTransformer.PreResult[AST.Exp] = {
+      o.attr.resOpt match {
+        case Some(res: AST.ResolvedInfo) =>
+          res match {
+            case m: AST.ResolvedInfo.Method if m.mode == AST.MethodMode.Constructor =>
+              val className = m.owner :+ m.id
+              if (funOwners.contains(className)) {
+                val ownerSimpleName = m.id
+                if (o.targs.size != 1) {
+                  reporter.error(o.posOpt, kind, s"$ownerSimpleName requires 1 type argument")
+                } else {
+                  val targ = o.targs(0)
+                  (o.args(0), o.args(2)) match {
+                    case (name: AST.Exp.LitString, fun: AST.Exp.Fun) if fun.params.size == 1 =>
+                      funs = funs + name.value ~> ((fun, targ))
+                    case (_: AST.Exp.LitString, fun) =>
+                      reporter.error(fun.posOpt, kind,
+                        s"Invalid ${funName.get(className).get} form for $ownerSimpleName; it has to be a function with a single parameter")
+                    case (name, _) =>
+                      reporter.error(name.posOpt, kind,
+                        s"Invalid name form for $ownerSimpleName; it has to be a String literal")
+                  }
+                }
+              }
+            case _ =>
+          }
+        case _ =>
+      }
+      return super.preExpInvoke(o)
+    }
+
+    override def preStmtEnum(o: AST.Stmt.Enum): AST.MTransformer.PreResult[AST.Stmt] = {
+      enums = enums + o.id.value ~> o
+      return super.preStmtEnum(o)
+    }
   }
 }
+
+import BitCodecGen._
 
 @record class BitCodecGen(licenseOpt: Option[String],
                           filename: String,
@@ -50,10 +115,61 @@ object BitCodecGen {
                           text: String,
                           config: Spec,
                           program: AST.TopUnit.Program,
-                          prevGen: String) {
-  def gen(): ST = {
-    println(config)
-    println(program)
-    return st""
+                          prevGen: String,
+                          reporter: Reporter) {
+  val collector: EnumFunCollector = EnumFunCollector(Reporter.create)
+  var userCodeMap: HashMap[String, String] = HashMap.empty
+
+  def gen(): String = {
+    collectEnumFun()
+    collectUserCode()
+    if (reporter.hasIssue) {
+      return prevGen
+    }
+    println("Coming soon ...")
+
+    return prevGen
+  }
+
+  def collectEnumFun(): Unit = {
+    collector.transformTopUnit(program)
+    reporter.reports(collector.reporter.messages)
+  }
+
+  def collectUserCode(): Unit = {
+    val lines = ops.StringOps(prevGen).split(c => c == '\n')
+    val size = lines.size
+    var i = 0
+    while (i < size) {
+      val line = lines(i)
+      val lOps = ops.StringOps(ops.StringOps(line).trim)
+      if (lOps.startsWith(beginCodeMarker)) {
+        val name = ops.StringOps(lOps.substring(beginCodeMarker.size, lOps.size)).trim
+        val beginLine = i
+        i = i + 1
+        var found = F
+        var code = ISZ[String]()
+        while (i < size && !found) {
+          val line2 = lines(i)
+          val lOps2 = ops.StringOps(ops.StringOps(line2).trim)
+          if (lOps2.startsWith(endCodeMarker)) {
+            found = T
+            val name2 = ops.StringOps(lOps2.substring(endCodeMarker.size, lOps2.size)).trim
+            if (name != name2) {
+              reporter.error(None(), kind, s"Mismatch code marker at lines $beginLine and $i ($name != $name2)")
+              return
+            }
+            userCodeMap = userCodeMap + name ~> st"${(code, "\n")}".render
+          } else {
+            code = code :+ line2
+          }
+          i = i + 1
+        }
+        if (!found) {
+          reporter.error(None(), kind, s"Unclosed code marker at line $beginLine for $name")
+        }
+      }
+      i = i + 1
+    }
   }
 }
