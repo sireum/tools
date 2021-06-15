840dd0e11e98200963c4c0786ce4200a934654db
Robby <robby@santoslab.org>
2021-06-15 08:31:48 -0500

    Added cligen support for generating script.

--- jvm/src/main/scala/org/sireum/tools/CliGenJvm.scala
+++ jvm/src/main/scala/org/sireum/tools/CliGenJvm.scala
@@ -30,6 +30,8 @@ import org.sireum._
 import org.sireum.cli.CliOpt
 
 object CliGenJvm {
+  val beginMarker: String = "// BEGIN USER CODE"
+  val endMarker: String = "// END USER CODE"
 
   def run(
     licenseOpt: Option[Os.Path],
@@ -46,8 +48,54 @@ object CliGenJvm {
       case _ => None()
     }
     val fOpt = Some(src.name)
-    return CliGen(firstColumnLimit, secondColumnLimit + firstColumnLimit)
-      .gen(lOpt, fOpt, packageName, nameOpt.getOrElse("Cli"), config)
+    val (preambleOpt, userCodeOpt): (Option[String], Option[String]) = if (dest.exists) {
+      val destContent = dest.read
+      val pOpt: Option[String] = if (dest.ext === "cmd") {
+        val destOps = ops.StringOps(destContent)
+        val endCmd: String = "::!#"
+        val i = destOps.stringIndexOf(endCmd)
+        if (i > 0) {
+          Some(s"${destOps.substring(0, i + endCmd.size)}\n")
+        } else {
+          Some(
+            st"""::#! 2> /dev/null                                   #
+                |@ 2>/dev/null # 2>nul & echo off & goto BOF         #
+                |if [ -z $${SIREUM_HOME} ]; then                      #
+                |  echo "Please set SIREUM_HOME env var"             #
+                |  exit -1                                           #
+                |fi                                                  #
+                |exec $${SIREUM_HOME}/bin/sireum slang run "$$0" "$$@"  #
+                |:BOF
+                |setlocal
+                |if not defined SIREUM_HOME (
+                |  echo Please set SIREUM_HOME env var
+                |  exit /B -1
+                |)
+                |%SIREUM_HOME%\bin\sireum.bat slang run "%0" %*
+                |exit /B %errorlevel%
+                |::!#
+                |""".render)
+        }
+      } else {
+        None()
+      }
+      val reporter = message.Reporter.create
+      val m = ops.StringOps(destContent).collectSections("cligen", beginMarker, endMarker, reporter)
+      m.size match {
+        case z"0" => (pOpt, None())
+        case z"1" => (pOpt, Some(m.values(0)))
+        case n => halt(s"Expecting at most one user code section, but found $n")
+      }
+    } else {
+      (None(), None())
+    }
+
+    var r = CliGen(firstColumnLimit, secondColumnLimit + firstColumnLimit)
+      .gen(preambleOpt, lOpt, fOpt, packageName, nameOpt.getOrElse("Cli"), config, beginMarker, endMarker, userCodeOpt)
       .render
+    if (dest.ext === "cmd") {
+      r = ops.StringOps(r).replaceAllLiterally("\n", "\r\n")
+    }
+    return r
   }
 }
--- shared/src/main/scala/org/sireum/tools/CliGen.scala
+++ shared/src/main/scala/org/sireum/tools/CliGen.scala
@@ -36,11 +36,15 @@ import org.sireum.cli.CliOpt._
   var enumNames: Set[String] = Set.empty
 
   def gen(
+    preambleOpt: Option[String],
     licenseOpt: Option[String],
     fileUriOpt: Option[String],
     packageNames: ISZ[String],
     name: String,
-    config: CliOpt
+    config: CliOpt,
+    beginMarker: String,
+    endMarker: String,
+    userCodeOpt: Option[String]
   ): ST = {
     val topName = s"${ops.StringOps(config.name).firstToUpper}TopOption"
 
@@ -48,200 +52,207 @@ import org.sireum.cli.CliOpt._
       case config: Group => group(topName, config)
       case config: Tool => tool(topName, config)
     }
-
-    val license: Option[ST] = licenseOpt.map((text: String) => st"""/*
-    | $text
-    | */
-    |""")
-    val fileUri: Option[ST] = fileUriOpt.map((text: String) => st"""// This file is auto-generated from $text
-    |""")
+    val license: Option[ST] = licenseOpt.map((text: String) =>
+      st"""/*
+          | $text
+          | */
+          |""")
+    val fileUri: Option[ST] = fileUriOpt.map((text: String) =>
+      st"""// This file is auto-generated from $text
+          |""")
     val packageName: Option[ST] =
-      if (packageNames.nonEmpty) Some(st"""package ${(packageNames, ".")}
-      |""") else None[ST]()
+      if (packageNames.nonEmpty) Some(
+        st"""package ${(packageNames, ".")}
+            |""")
+      else None[ST]()
     val r =
-      st"""// #Sireum
-      |// @formatter:off
-      |
-      |$license
-      |$fileUri
-      |$packageName
-      |import org.sireum._
-      |
-      |object $name {
-      |
-      |  @datatype trait $topName
-      |
-      |  @datatype class HelpOption extends $topName
-      |
-      |  ${(decls, "\n\n")}
-      |}
-      |
-      |import $name._
-      |
-      |@record class $name(pathSep: C) {
-      |
-      |  ${(parser, "\n\n")}
-      |
-      |  def parseArguments(args: ISZ[String], i: Z): ISZ[String] = {
-      |    var r = ISZ[String]()
-      |    var j = i
-      |    while (j < args.size) {
-      |      r = r :+ args(j)
-      |      j = j + 1
-      |    }
-      |    return r
-      |  }
-      |
-      |  def parsePaths(args: ISZ[String], i: Z): Option[ISZ[String]] = {
-      |    return tokenize(args, i, "path", pathSep, F)
-      |  }
-      |
-      |  def parsePath(args: ISZ[String], i: Z): Option[Option[String]] = {
-      |    if (i >= args.size) {
-      |      eprintln("Expecting a path, but none found.")
-      |    }
-      |    return Some(Some(args(i)))
-      |  }
-      |
-      |  def parseStrings(args: ISZ[String], i: Z, sep: C): Option[ISZ[String]] = {
-      |    tokenize(args, i, "string", sep, F) match {
-      |      case r@Some(_) => return r
-      |      case _ => return None()
-      |    }
-      |  }
-      |
-      |  def parseString(args: ISZ[String], i: Z): Option[Option[String]] = {
-      |    if (i >= args.size) {
-      |      eprintln("Expecting a string, but none found.")
-      |      return None()
-      |    }
-      |    return Some(Some(args(i)))
-      |  }
-      |
-      |  def parseNums(args: ISZ[String], i: Z, sep: C, minOpt: Option[Z], maxOpt: Option[Z]): Option[ISZ[Z]] = {
-      |    tokenize(args, i, "integer", sep, T) match {
-      |      case Some(sargs) =>
-      |        var r = ISZ[Z]()
-      |        for (arg <- sargs) {
-      |          parseNumH(arg, minOpt, maxOpt) match {
-      |            case Some(n) => r = r :+ n
-      |            case _ => return None()
-      |          }
-      |        }
-      |        return Some(r)
-      |      case _ => return None()
-      |    }
-      |  }
-      |
-      |  def tokenize(args: ISZ[String], i: Z, tpe: String, sep: C, removeWhitespace: B): Option[ISZ[String]] = {
-      |    if (i >= args.size) {
-      |      eprintln(s"Expecting a sequence of $$tpe separated by '$$sep', but none found.")
-      |      return None()
-      |    }
-      |    val arg = args(i)
-      |    return Some(tokenizeH(arg, sep, removeWhitespace))
-      |  }
-      |
-      |  def tokenizeH(arg: String, sep: C, removeWhitespace: B): ISZ[String] = {
-      |    val argCis = conversions.String.toCis(arg)
-      |    var r = ISZ[String]()
-      |    var cis = ISZ[C]()
-      |    var j = 0
-      |    while (j < argCis.size) {
-      |      val c = argCis(j)
-      |      if (c == sep) {
-      |        r = r :+ conversions.String.fromCis(cis)
-      |        cis = ISZ[C]()
-      |      } else {
-      |        val allowed: B = c match {
-      |          case c"\n" => !removeWhitespace
-      |          case c" " => !removeWhitespace
-      |          case c"\r" => !removeWhitespace
-      |          case c"\t" => !removeWhitespace
-      |          case _ => T
-      |        }
-      |        if (allowed) {
-      |          cis = cis :+ c
-      |        }
-      |      }
-      |      j = j + 1
-      |    }
-      |    if (cis.size > 0) {
-      |      r = r :+ conversions.String.fromCis(cis)
-      |    }
-      |    return r
-      |  }
-      |
-      |  def parseNumChoice(args: ISZ[String], i: Z, choices: ISZ[Z]): Option[Z] = {
-      |    val set = HashSet.empty[Z] ++ choices
-      |    parseNum(args, i, None(), None()) match {
-      |      case r@Some(n) =>
-      |        if (set.contains(n)) {
-      |          return r
-      |        } else {
-      |          eprintln(s"Expecting one of the following: $$set, but found $$n.")
-      |          return None()
-      |        }
-      |      case r => return r
-      |    }
-      |  }
-      |
-      |  def parseNum(args: ISZ[String], i: Z, minOpt: Option[Z], maxOpt: Option[Z]): Option[Z] = {
-      |    if (i >= args.size) {
-      |      eprintln(s"Expecting an integer, but none found.")
-      |      return None()
-      |    }
-      |    return parseNumH(args(i), minOpt, maxOpt)
-      |  }
-      |
-      |  def parseNumH(arg: String, minOpt: Option[Z], maxOpt: Option[Z]): Option[Z] = {
-      |    Z(arg) match {
-      |      case Some(n) =>
-      |        minOpt match {
-      |          case Some(min) =>
-      |            if (n < min) {
-      |              eprintln(s"Expecting an integer at least $$min, but found $$n.")
-      |              return None()
-      |            }
-      |          case _ =>
-      |        }
-      |        maxOpt match {
-      |          case Some(max) =>
-      |            if (n > max) {
-      |              eprintln(s"Expecting an integer at most $$max, but found $$n.")
-      |              return None()
-      |            }
-      |            return Some(n)
-      |          case _ =>
-      |        }
-      |        return Some(n)
-      |      case _ =>
-      |        eprintln(s"Expecting an integer, but found '$$arg'.")
-      |        return None()
-      |    }
-      |  }
-      |
-      |  def select(mode: String, args: ISZ[String], i: Z, choices: ISZ[String]): Option[String] = {
-      |    val arg = args(i)
-      |    var cs = ISZ[String]()
-      |    for (c <- choices) {
-      |      if (ops.StringOps(c).startsWith(arg)) {
-      |        cs = cs :+ c
-      |      }
-      |    }
-      |    cs.size match {
-      |      case z"0" =>
-      |        eprintln(s"$$arg is not a mode of $$mode.")
-      |        return None()
-      |      case z"1" => return Some(cs(0))
-      |      case _ =>
-      |        eprintln(
-      |          st${tqs}Which one of the following modes did you mean by '$$arg'?
-      |              |$${(cs, "\n")}$tqs.render)
-      |        return None()
-      |    }
-      |  }
-      |}"""
+      st"""$preambleOpt// #Sireum
+          |// @formatter:off
+          |
+          |$license
+          |$fileUri
+          |$packageName
+          |import org.sireum._
+          |
+          |$beginMarker
+          |$userCodeOpt
+          |$endMarker
+          |
+          |object $name {
+          |
+          |  @datatype trait $topName
+          |
+          |  @datatype class HelpOption extends $topName
+          |
+          |  ${(decls, "\n\n")}
+          |}
+          |
+          |import $name._
+          |
+          |@record class $name(pathSep: C) {
+          |
+          |  ${(parser, "\n\n")}
+          |
+          |  def parseArguments(args: ISZ[String], i: Z): ISZ[String] = {
+          |    var r = ISZ[String]()
+          |    var j = i
+          |    while (j < args.size) {
+          |      r = r :+ args(j)
+          |      j = j + 1
+          |    }
+          |    return r
+          |  }
+          |
+          |  def parsePaths(args: ISZ[String], i: Z): Option[ISZ[String]] = {
+          |    return tokenize(args, i, "path", pathSep, F)
+          |  }
+          |
+          |  def parsePath(args: ISZ[String], i: Z): Option[Option[String]] = {
+          |    if (i >= args.size) {
+          |      eprintln("Expecting a path, but none found.")
+          |    }
+          |    return Some(Some(args(i)))
+          |  }
+          |
+          |  def parseStrings(args: ISZ[String], i: Z, sep: C): Option[ISZ[String]] = {
+          |    tokenize(args, i, "string", sep, F) match {
+          |      case r@Some(_) => return r
+          |      case _ => return None()
+          |    }
+          |  }
+          |
+          |  def parseString(args: ISZ[String], i: Z): Option[Option[String]] = {
+          |    if (i >= args.size) {
+          |      eprintln("Expecting a string, but none found.")
+          |      return None()
+          |    }
+          |    return Some(Some(args(i)))
+          |  }
+          |
+          |  def parseNums(args: ISZ[String], i: Z, sep: C, minOpt: Option[Z], maxOpt: Option[Z]): Option[ISZ[Z]] = {
+          |    tokenize(args, i, "integer", sep, T) match {
+          |      case Some(sargs) =>
+          |        var r = ISZ[Z]()
+          |        for (arg <- sargs) {
+          |          parseNumH(arg, minOpt, maxOpt) match {
+          |            case Some(n) => r = r :+ n
+          |            case _ => return None()
+          |          }
+          |        }
+          |        return Some(r)
+          |      case _ => return None()
+          |    }
+          |  }
+          |
+          |  def tokenize(args: ISZ[String], i: Z, tpe: String, sep: C, removeWhitespace: B): Option[ISZ[String]] = {
+          |    if (i >= args.size) {
+          |      eprintln(s"Expecting a sequence of $$tpe separated by '$$sep', but none found.")
+          |      return None()
+          |    }
+          |    val arg = args(i)
+          |    return Some(tokenizeH(arg, sep, removeWhitespace))
+          |  }
+          |
+          |  def tokenizeH(arg: String, sep: C, removeWhitespace: B): ISZ[String] = {
+          |    val argCis = conversions.String.toCis(arg)
+          |    var r = ISZ[String]()
+          |    var cis = ISZ[C]()
+          |    var j = 0
+          |    while (j < argCis.size) {
+          |      val c = argCis(j)
+          |      if (c == sep) {
+          |        r = r :+ conversions.String.fromCis(cis)
+          |        cis = ISZ[C]()
+          |      } else {
+          |        val allowed: B = c match {
+          |          case c"\n" => !removeWhitespace
+          |          case c" " => !removeWhitespace
+          |          case c"\r" => !removeWhitespace
+          |          case c"\t" => !removeWhitespace
+          |          case _ => T
+          |        }
+          |        if (allowed) {
+          |          cis = cis :+ c
+          |        }
+          |      }
+          |      j = j + 1
+          |    }
+          |    if (cis.size > 0) {
+          |      r = r :+ conversions.String.fromCis(cis)
+          |    }
+          |    return r
+          |  }
+          |
+          |  def parseNumChoice(args: ISZ[String], i: Z, choices: ISZ[Z]): Option[Z] = {
+          |    val set = HashSet.empty[Z] ++ choices
+          |    parseNum(args, i, None(), None()) match {
+          |      case r@Some(n) =>
+          |        if (set.contains(n)) {
+          |          return r
+          |        } else {
+          |          eprintln(s"Expecting one of the following: $$set, but found $$n.")
+          |          return None()
+          |        }
+          |      case r => return r
+          |    }
+          |  }
+          |
+          |  def parseNum(args: ISZ[String], i: Z, minOpt: Option[Z], maxOpt: Option[Z]): Option[Z] = {
+          |    if (i >= args.size) {
+          |      eprintln(s"Expecting an integer, but none found.")
+          |      return None()
+          |    }
+          |    return parseNumH(args(i), minOpt, maxOpt)
+          |  }
+          |
+          |  def parseNumH(arg: String, minOpt: Option[Z], maxOpt: Option[Z]): Option[Z] = {
+          |    Z(arg) match {
+          |      case Some(n) =>
+          |        minOpt match {
+          |          case Some(min) =>
+          |            if (n < min) {
+          |              eprintln(s"Expecting an integer at least $$min, but found $$n.")
+          |              return None()
+          |            }
+          |          case _ =>
+          |        }
+          |        maxOpt match {
+          |          case Some(max) =>
+          |            if (n > max) {
+          |              eprintln(s"Expecting an integer at most $$max, but found $$n.")
+          |              return None()
+          |            }
+          |            return Some(n)
+          |          case _ =>
+          |        }
+          |        return Some(n)
+          |      case _ =>
+          |        eprintln(s"Expecting an integer, but found '$$arg'.")
+          |        return None()
+          |    }
+          |  }
+          |
+          |  def select(mode: String, args: ISZ[String], i: Z, choices: ISZ[String]): Option[String] = {
+          |    val arg = args(i)
+          |    var cs = ISZ[String]()
+          |    for (c <- choices) {
+          |      if (ops.StringOps(c).startsWith(arg)) {
+          |        cs = cs :+ c
+          |      }
+          |    }
+          |    cs.size match {
+          |      case z"0" =>
+          |        eprintln(s"$$arg is not a mode of $$mode.")
+          |        return None()
+          |      case z"1" => return Some(cs(0))
+          |      case _ =>
+          |        eprintln(
+          |          st${tqs}Which one of the following modes did you mean by '$$arg'?
+          |              |$${(cs, "\n")}$tqs.render)
+          |        return None()
+          |    }
+          |  }
+          |}"""
     return r
   }
 
