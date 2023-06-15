7cb949b990c41484cc6e844c711821022ae0a71e
Jacob-Armm <jdlegg@ksu.edu>
2023-06-15 13:13:53 -0500

    Implmented slangcheck

--- jvm/src/test/scala/org/sireum/tools/slangcheck/SlangCheckTest.scala
+++ jvm/src/test/scala/org/sireum/tools/slangcheck/SlangCheckTest.scala
@@ -1,5 +1,13 @@
 package org.sireum.tools.slangcheck
 
-class SlangCheckTest {
+import org.sireum.$internal.RC
+import org.sireum._
+import org.sireum.lang.parser.Parser
+import org.sireum.lang.tipe.{PostTipeAttrChecker, TypeChecker, TypeHierarchy}
+import org.sireum.lang.{FrontEnd, ast => AST}
+import org.sireum.message.Reporter
+import org.sireum.test.TestSuite
+
+class SlangCheckTest extends TestSuite {
 
 }
