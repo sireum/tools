e320d945928728e105bb80f4e8366a7c71f03d08
Jason Belt <belt@ksu.edu>
2023-08-29 11:30:46 -0500

    output to slang dir

--- jvm/src/test/scala/org/sireum/tools/slangcheck/SlangCheckHAMRTest.scala
+++ jvm/src/test/scala/org/sireum/tools/slangcheck/SlangCheckHAMRTest.scala
@@ -31,16 +31,18 @@ class SlangCheckHAMRTest extends TestSuite with TestUtil {
     val json = (resourceDir / projName / "aadl" / ".slang").list.filter(p => p.ext == string"json")
     assert (json.size == 1)
 
+    val outDir = resultsDir / "hamr" / "slang"
+
     println("Running codegen ...")
-    proc"$sireum hamr codegen --no-proyek-ive --package-name $packageName --output-dir $resultsDir ${json(0)}".echo.console.runCheck()
+    proc"$sireum hamr codegen --no-proyek-ive --package-name $packageName --output-dir $outDir ${json(0)}".echo.console.runCheck()
 
     val files = {
-      val slangcheckBin = ops.StringOps((resultsDir / "bin" / "slangcheck.cmd").read)
+      val slangcheckBin = ops.StringOps((outDir / "bin" / "slangcheck.cmd").read)
       val searchString = "val files: ISZ[String] = ISZ("
       val start = slangcheckBin.stringIndexOf(searchString)
       def toFile(s: String): Os.Path = {
         val ss = ops.StringOps(ops.StringOps(s).trim)
-        return resultsDir / ss.substring(4, ss.stringIndexOf("scala") + 5)
+        return outDir / ss.substring(4, ss.stringIndexOf("scala") + 5)
       }
       for (file <- ops.StringOps(ops.StringOps(slangcheckBin.substring(start + searchString.length, slangcheckBin.stringIndexOfFrom(")", start))).replaceAllLiterally("\r\n", "|")).split(c => c == C('|'))) yield toFile(file)
     }
@@ -51,7 +53,7 @@ class SlangCheckHAMRTest extends TestSuite with TestUtil {
 
     assert (!reporter.hasError, reporter.errors)
 
-    val dataDir = resultsDir / "src" / "main" / "data" / packageName
+    val dataDir = outDir / "src" / "main" / "data" / packageName
     for (r <- results._1) {
       val destFile = dataDir /+ r._1
       assert (destFile.exists)
@@ -60,9 +62,9 @@ class SlangCheckHAMRTest extends TestSuite with TestUtil {
     }
 
     println("Running tipe ...")
-    proc"$sireum proyek tipe .".at(resultsDir).echo.console.runCheck()
+    proc"$sireum proyek tipe .".at(outDir).echo.console.runCheck()
 
     println("Compiling ...")
-    proc"$sireum proyek compile .".at(resultsDir).echo.console.runCheck()
+    proc"$sireum proyek compile .".at(outDir).echo.console.runCheck()
   }
 }
