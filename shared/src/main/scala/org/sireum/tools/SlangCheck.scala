440761676ebf4e4e9ad0f987bc0b0456c36d844a
jdlegg <jdlegg@ksu.edu>
2023-10-12 09:39:44 -0500

    removed gen from nextString

--- shared/src/main/scala/org/sireum/tools/SlangCheck.scala
+++ shared/src/main/scala/org/sireum/tools/SlangCheck.scala
@@ -320,7 +320,7 @@ object SlangCheckTest {
           |    val length: Z = gen.nextZBetween(0, get_numElement)
           |    var str: String = ""
           |    for(r <- 0 until length){
-          |      str = s"$${str}$${gen.nextC().string}"
+          |      str = s"$${str}$${nextC().string}"
           |    }
           |
           |    return str
