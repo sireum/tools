0e61074985c256a2d5782e37398ebb4f24e88e2e
Robby <robby@santoslab.org>
2024-09-11 10:14:38 -0500

    Addressed whitespace issues in SIREUM_HOME path.

--- jvm/src/main/scala/org/sireum/tools/CliGenJvm.scala
+++ jvm/src/main/scala/org/sireum/tools/CliGenJvm.scala
@@ -65,14 +65,14 @@ object CliGenJvm {
                 |  echo "Please set SIREUM_HOME env var"             #
                 |  exit -1                                           #
                 |fi                                                  #
-                |exec $${SIREUM_HOME}/bin/sireum slang run "$$0" "$$@"  #
+                |exec "$${SIREUM_HOME}/bin/sireum slang run" "$$0" "$$@"  #
                 |:BOF
                 |setlocal
                 |if not defined SIREUM_HOME (
                 |  echo Please set SIREUM_HOME env var
                 |  exit /B -1
                 |)
-                |%SIREUM_HOME%\bin\sireum.bat slang run "%0" %*
+                |"%SIREUM_HOME%\bin\sireum.bat" slang run "%0" %*
                 |exit /B %errorlevel%
                 |::!#*/
                 |""".render)
--- jvm/src/test/resources/org/sireum/tools/slangcheck/temp_control/bin/project.cmd
+++ jvm/src/test/resources/org/sireum/tools/slangcheck/temp_control/bin/project.cmd
@@ -4,14 +4,14 @@ if [ -z ${SIREUM_HOME} ]; then                        #
   echo "Please set SIREUM_HOME env var"               #
   exit -1                                             #
 fi                                                    #
-exec ${SIREUM_HOME}/bin/sireum slang run "$0" "$@"    #
+exec "${SIREUM_HOME}/bin/sireum" slang run "$0" "$@"  #
 :BOF
 setlocal
 if not defined SIREUM_HOME (
   echo Please set SIREUM_HOME env var
   exit /B -1
 )
-%SIREUM_HOME%\\bin\\sireum.bat slang run "%0" %*
+"%SIREUM_HOME%\bin\sireum.bat" slang run "%0" %*
 exit /B %errorlevel%
 ::!#*/
 // #Sireum
